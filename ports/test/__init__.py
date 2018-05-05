3ce5b8b7b80e7b378e7773cc038d9b305848a081
jinxes <blldxt@yahoo.com>
2018-05-06 00:19:17 +0800

    delete test

--- ports/test/__init__.py
+++
@@ -1,6 +0,0 @@
-from boot import app
-from flask import Blueprint
-from ports.user.api.index import Controller
-
-blueprint = Blueprint('test', __name__, url_prefix='/test/')
-blueprint.add_url_rule('/', 'test_index', Controller.as_view('test'))
--- ports/test/api/index.py
+++
@@ -1,8 +0,0 @@
-from flask.views import View
-
-class Controller(View):
-
-    methods = ['GET']
-
-    def dispatch_request(self):
-        return 'Hello World'
